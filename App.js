b724c48318b4d48a21567b01ca6efce111ebde4a
Marlus Freitas <marlus_fox@hotmail.com>
2023-11-30 16:07:56 -0300

    Added NewGoal and Schedules screens

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GoalsScreen } from './src/routes/screens/GoalsScreen';
 import { DetailGoalScreen } from './src/routes/screens/DetailGoalScreen';
 import { StatusBar } from 'expo-status-bar';
+import { NewGoalScreen } from './src/routes/screens/NewGoalScreen';
+import { ScheduleScreen } from './src/routes/screens/SchedulesScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +26,10 @@ function App() {
           <Stack.Screen name="Goals" component={GoalsScreen} options={screenOptions} />
           
           <Stack.Screen name="DetailGoal" component={DetailGoalScreen} options={screenOptions} />
+
+          <Stack.Screen name="NewGoal" component={NewGoalScreen} options={screenOptions} />
+          
+          <Stack.Screen name="Schedules" component={ScheduleScreen} options={screenOptions} />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style="light" />
